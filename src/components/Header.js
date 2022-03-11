41d98da79baaae5f1db52cb1d5606763b35b303b
jean00 <jeanlouis433@gmail.com>
2022-03-11 18:07:58 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="titles">
+          <h1 className="title"> CV Application </h1>
+          <h3 className="subtitle"> Create you cv application</h3>
+        </div>
+      </header>
+    );
+  }
+}
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <section className="cv">
+          <div className="doc"></div>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Main;
